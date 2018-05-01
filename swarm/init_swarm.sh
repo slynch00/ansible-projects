ca7b42c3b45d110af0dbc2de43754fd52f2c8aef
Lynch, Steve <steve1.lynch@dish.com>
2018-05-01 07:43:56 -0600

    upload templates

--- swarm/init_swarm.sh
+++ swarm/init_swarm.sh
@@ -7,30 +7,25 @@ jq -r ".[] | .ansible_facts.workers" /root/worker_list > /root/workers
 # get swarm manager token
 ###########################
 docker-machine ssh leader_address > /root/swarm_manager_token 2>&1 << EOF
-#docker-machine ssh docker-machine-manager-test-01 > /root/swarm_manager_token 2>&1 << EOF
   sudo su -
   docker swarm init --advertise-addr eth0
-  sleep 3
-  SMGR=$(docker swarm join-token manager | grep docker)
-  echo $SMGR
+  docker swarm join-token manager | grep docker
   exit
   exit
 EOF
-TOKEN=$(grep "docker swarm join --token manager" /root/swarm_manager_token)
-
+TOKEN=$(grep "docker swarm join --token" /root/swarm_manager_token | tail -1)
+echo MANAGER_TOKEN=$TOKEN
 ###########################
 # get swarm worker token
 ###########################
 docker-machine ssh leader_address > /root/swarm_worker_token 2>&1 << EOF
-#docker-machine ssh docker-machine-manager-test-01 > /root/swarm_worker_token 2>&1 << EOF
   sudo su -
-  SWKR=$(docker swarm join-token worker | grep docker)
-  echo $SWKR
+  docker swarm join-token worker | grep docker
   exit
   exit
 EOF
 WTOKEN=$(grep "docker swarm join --token" /root/swarm_worker_token)
-
+echo WORKER_TOKEN=$WTOKEN
 ###########################
 # join docker-machine managers to swarm
 ###########################
@@ -58,9 +53,3 @@ docker-machine ssh $w > /root/worker-temp-results 2>&1 << EOF
   exit
 EOF
 done </root/workers
-
-###########################
-# clean up temp files
-###########################
-#rm -f /root/swarm_manager_cmd
-#rm -f /root/manager_list
